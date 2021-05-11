c639468594157e2d4be4759004a7ea6b5a1a471a
samuel <pastelmaker@gmail.com>
2021-05-11 14:07:21 +0700

    Jumbotron slider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css'
 import Slider from './components/Slider'
-import Jumbotron from './components/Jumbotron'
 import About from './components/About'
 
 function App() {
   return (
     <div>
       <Slider />
-      <Jumbotron />
       <About />
     </div>
   )
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,9 +3,12 @@ import { useState } from 'react'
 import slidesItem1 from '../images/mobile-image-hero-1.jpg'
 import slidesItem2 from '../images/mobile-image-hero-2.jpg'
 import slidesItem3 from '../images/mobile-image-hero-3.jpg'
+import jumbotronSlides from '../data/jumbotron'
 
 import { ReactComponent as PrevSlideIcon } from '../images/icon-angle-left.svg'
 import { ReactComponent as NextSlideIcon } from '../images/icon-angle-right.svg'
+import { ReactComponent as ArrowIcon } from '../images/icon-arrow.svg'
+
 import styles from './Slider.module.css'
 
 function Slider() {
@@ -25,37 +28,58 @@ function Slider() {
   }
 
   return (
-    <div className={styles.slider}>
-      <div
-        className={styles.sliderContainer}
-        style={{
-          transform: `translateX(${activeSliderIdx * -100}%)`
-        }}
-      >
-        {slides.map((slide, index) => (
-          <img
-            key={`slider-item-${index}`}
-            src={slide}
-            alt={`slider-item-${index}`}
-            className={styles.sliderItem}
-          />
-        ))}
+    <div>
+      <div className={styles.slider}>
+        <div
+          className={styles.sliderContainer}
+          style={{
+            transform: `translateX(${activeSliderIdx * -100}%)`
+          }}
+        >
+          {slides.map((slide, index) => (
+            <img
+              key={`slider-item-${index}`}
+              src={slide}
+              alt={`slider-item-${index}`}
+              className={styles.sliderItem}
+            />
+          ))}
+        </div>
+        <div className={styles.sliderNavigationContainer}>
+          <button
+            className={styles.sliderNavigation}
+            type="button"
+            onClick={() => goTo('prev')}
+          >
+            <PrevSlideIcon />
+          </button>
+          <button
+            className={styles.sliderNavigation}
+            type="button"
+            onClick={() => goTo('next')}
+          >
+            <NextSlideIcon />
+          </button>
+        </div>
       </div>
-      <div className={styles.sliderNavigationContainer}>
-        <button
-          className={styles.sliderNavigation}
-          type="button"
-          onClick={() => goTo('prev')}
+      <div className={styles.jumbotronSlider}>
+        <div
+          className={styles.jumbotronSliderContainer}
+          style={{
+            transform: `translateX(${activeSliderIdx * -100}%)`
+          }}
         >
-          <PrevSlideIcon />
-        </button>
-        <button
-          className={styles.sliderNavigation}
-          type="button"
-          onClick={() => goTo('next')}
-        >
-          <NextSlideIcon />
-        </button>
+          {jumbotronSlides.map((jumbotron) => (
+            <div className={styles.jumbotronSliderItem}>
+              <h2 className="main-heading">{jumbotron.title}</h2>
+              <p className="default-paragraph mb-0">{jumbotron.paragraph}</p>
+              <a className={styles.jumbotronLink} href="#">
+                <span>SHOP NOW</span>
+                <ArrowIcon style={{ marginLeft: '40px' }} />
+              </a>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
